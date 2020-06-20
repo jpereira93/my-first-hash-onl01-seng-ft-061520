60b9cedd931fec722ecb3e4b440c33f0dc622075
Jessica Pereira <jcpereira070@gmail.com>
2020-06-20 21:11:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,10 +29,11 @@ def adding
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3
+                }
 
                   shipping_manifest["muskets"] = 2
 
                   shipping_manifest["gun powder"] = 4
-shipping_manifest
+                  shipping_manifest
 
 end
